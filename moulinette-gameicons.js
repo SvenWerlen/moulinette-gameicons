51f5261eafc86b0ecff14192a051c373a5fa2d76
Sven Werlen <sven.werlen@pm.me>
2021-04-16 22:27:55 -0400

    Finalizing v1

--- moulinette-gameicons.js
+++ moulinette-gameicons.js
@@ -13,6 +13,7 @@ Hooks.once("init", async function () {
 Hooks.once("ready", async function () {
   if (game.user.isGM) {
     // create default home folder for game icons
+    await game.moulinette.applications.MoulinetteFileUtil.createFolderIfMissing(".", "moulinette");
     await game.moulinette.applications.MoulinetteFileUtil.createFolderIfMissing("moulinette", "moulinette/images");
     await game.moulinette.applications.MoulinetteFileUtil.createFolderIfMissing("moulinette/images", "moulinette/images/gameicons");
     
@@ -30,6 +31,6 @@ Hooks.once("ready", async function () {
       ]
     })
         
-    console.log("Moulinette Scenes | Module loaded")
+    console.log("Moulinette GameIcons | Module loaded")
   }
 });
