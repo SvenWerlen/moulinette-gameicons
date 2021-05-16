7028d7609864cb0b73c16faf4f77a22e47579dfd
Sven Werlen <sven.werlen@pm.me>
2021-05-15 23:20:49 -0400

    Fix for Firefox (width/height)

--- modules/moulinette-gameicons.js
+++ modules/moulinette-gameicons.js
@@ -141,6 +141,9 @@ export class MoulinetteGameIcons extends game.moulinette.applications.Moulinette
         imageName = svg.split('/').pop() + `-${fgColor}-${bgColor}.svg`
       }
       
+      // fix for Firefox users => add widths
+      text = text.replace("<svg", `<svg width="200" height="200"`)
+      
       lastResponse = await game.moulinette.applications.MoulinetteFileUtil.upload(new File([text], imageName, { type: "image/svg+xml", lastModified: new Date() }), imageName, "moulinette/images", `moulinette/images/gameicons`, true)
       SceneNavigation._onLoadProgress(game.i18n.localize("mtte.installingGameIcons"), Math.round((idx / selected.length)*100));
     }
