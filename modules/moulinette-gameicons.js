07ed43672328a5e6715f5cfe2089564e2adce17b
Sven Werlen <sven.werlen@pm.me>
2022-06-10 22:32:51 -0400

    Color presets

--- modules/moulinette-gameicons.js
+++ modules/moulinette-gameicons.js
@@ -6,6 +6,7 @@ export class MoulinetteGameIcons extends game.moulinette.applications.Moulinette
   constructor() {
     super()
     this.scenes = []
+    this.presets = []
   }
   
   supportsModes() { return false }
@@ -58,8 +59,17 @@ export class MoulinetteGameIcons extends game.moulinette.applications.Moulinette
     // keep html for later usage
     this.html = html
     
+    // preset selected
+    html.find(".options select").change(this._onPreset.bind(this))
+
     // enable alt _alternateColors
     this._alternateColors()
+
+    // fill color presets
+    this.resetPresets()
+
+    // game icons has a larger footer
+    html.find(".list").css("bottom", "188px")
   }
   
   /**
@@ -69,10 +79,51 @@ export class MoulinetteGameIcons extends game.moulinette.applications.Moulinette
     const fgColor = game.settings.get("moulinette", "gIconFgColor")
     const bgColor = game.settings.get("moulinette", "gIconBgColor")
     const compact = game.settings.get("moulinette-core", "uiMode") == "compact"
-    return `<div class="options">
-      ${compact ? "" : game.i18n.localize("mtte.foregroundColor")} <input class="color" type="text" name="fgColor" maxlength="7" value="${fgColor}"> <input type="color" value="${fgColor}" data-edit="fgColor">
-      ${compact ? "" : game.i18n.localize("mtte.backgroundColor")} <input class="color" type="text" name="bgColor" maxlength="7" value="${bgColor}"> <input type="color" value="${bgColor}" data-edit="bgColor"> 
-    </div>`
+    let footer = `<div class="options">
+      ${compact ? "" : game.i18n.localize("mtte.foregroundColor")} <input class="color" type="text" name="fgColor" maxlength="7" value="${fgColor}"> <input name="fgColorPicker" type="color" value="${fgColor}" data-edit="fgColor">
+      ${compact ? "" : game.i18n.localize("mtte.backgroundColor")} <input class="color" type="text" name="bgColor" maxlength="7" value="${bgColor}"> <input name="bgColorPicker" type="color" value="${bgColor}" data-edit="bgColor">
+      <button class="save">${game.i18n.localize("mtte.saveaspreset")}</button>`
+    // add presets
+    footer += `<br />${compact ? "" : game.i18n.localize("mtte.preset")} <select class="presets"><option value="">--</option>`
+    for(let i = 0; i < this.presets.length; i++) {
+      footer += `<option value="${i+1}">${this.presets[i].name}</option>`
+    }
+    footer += `</select> <button class="deletePreset">${game.i18n.localize("mtte.deletepreset")}</button>`
+    footer += "</div>"
+    return footer
+  }
+
+  resetPresets() {
+    this.presets = []
+    this.presets.push({name: game.i18n.localize("mtte.presetWhiteBlack"), fg: "#ffffff", bg: "#000000"})
+    this.presets.push({name: game.i18n.localize("mtte.presetBlackWhite"), fg: "#000000", bg: "#ffffff"})
+    this.presets.push({name: game.i18n.localize("mtte.presetBlackTransparent"), fg: "#000000", bg: ""})
+    this.basePresetsCount = this.presets.length
+    const presets = game.settings.get("moulinette", "gIconPresets")
+    for(const p of presets) {
+      this.presets.push(p)
+    }
+    // prepare HTML
+    let select = `<select class="presets"><option value="">--</option>`
+    for(let i = 0; i < this.presets.length; i++) {
+      select += `<option value="${i+1}">${this.presets[i].name}</option>`
+    }
+    select += "</select>"
+    this.html.find("select").html(select)
+
+    // hide delete button
+    this.html.find(".deletePreset").css("visibility", "hidden");
+  }
+
+  _onPreset() {
+    const idx = parseInt(this.html.find(".presets :selected").val())
+    if(idx > 0 && idx <= this.presets.length) {
+      this.html.find(".deletePreset").css("visibility", idx <= this.basePresetsCount ? "hidden" : "visible");
+      this.html.find("input[name='fgColor']").val(this.presets[idx-1].fg)
+      this.html.find("input[name='fgColorPicker']").val(this.presets[idx-1].fg)
+      this.html.find("input[name='bgColor']").val(this.presets[idx-1].bg)
+      this.html.find("input[name='bgColorPicker']").val(this.presets[idx-1].bg)
+    }
   }
   
   _alternateColors() {
@@ -103,6 +154,37 @@ export class MoulinetteGameIcons extends game.moulinette.applications.Moulinette
     } 
     else if(classList.contains("howto")) {
       new game.moulinette.applications.MoulinetteHelp("icons").render(true)
+    }
+    else if(classList.contains("save")) {
+      const parent = this
+      Dialog.prompt({
+        title: game.i18n.localize("mtte.presetName"),
+        content: ` <input type="text" placeholder="${game.i18n.localize("mtte.enterPresetName")}">`,
+        callback: (html) => {
+          const label = html.find('input').val()
+          if(label && label.length > 0) {
+            const fgColor = this.html.find("input[name='fgColor']").val()
+            const bgColor = this.html.find("input[name='bgColor']").val()
+
+            const list = game.settings.get("moulinette", "gIconPresets")
+            list.push({name: label, fg: fgColor, bg: bgColor})
+            list.sort(function(a, b) { return ('' + a.name).localeCompare(b.name); });
+            game.settings.set("moulinette", "gIconPresets", list).then((a) => { parent.resetPresets() })
+          }
+        }
+      })
+    }
+    else if(classList.contains("deletePreset")) {
+      const parent = this
+      const idx = parseInt(this.html.find(".presets :selected").val())
+      if(idx > 0 && idx <= this.presets.length) {
+        // remove selected index from list
+        // idx = index in drop-down list
+        // basePresetsCount = number of base presets (which are not stored in prefs)
+        let list = game.settings.get("moulinette", "gIconPresets")
+        list.splice(idx-this.basePresetsCount-1, 1)
+        game.settings.set("moulinette", "gIconPresets", list).then((a) => { parent.resetPresets() })
+      }
     }
     else {
       console.warn(`MoulinetteGameIcons | No action implemented for action '${classList}'`)
--- moulinette-gameicons.js
+++ moulinette-gameicons.js
@@ -4,6 +4,7 @@ Hooks.once("init", async function () {
   console.log("Moulinette GameIcons | Init")
   game.settings.register("moulinette", "gIconFgColor", { scope: "world", config: false, type: String, default: "#ffffff" })
   game.settings.register("moulinette", "gIconBgColor", { scope: "world", config: false, type: String, default: "#000000" })
+  game.settings.register("moulinette", "gIconPresets", { scope: "world", config: false, type: Object, default: [] })
 });
 
 
@@ -25,7 +26,7 @@ Hooks.once("ready", async function () {
       actions: [
         {id: "clear", icon: "far fa-square" ,name: game.i18n.localize("mtte.clearAction"), help: game.i18n.localize("mtte.clearActionToolTip") },
         {id: "selectAll", icon: "far fa-check-square" ,name: game.i18n.localize("mtte.selectAllAction"), help: game.i18n.localize("mtte.selectAllActionToolTip") },
-        {id: "install", icon: "fas fa-hammer" ,name: game.i18n.localize("mtte.forge"), help: game.i18n.localize("mtte.forgeToolTip") },
+        {id: "install", icon: "fas fa-download" ,name: game.i18n.localize("mtte.forge"), help: game.i18n.localize("mtte.forgeToolTip") },
         {id: "howto", icon: "fas fa-question-circle" ,name: game.i18n.localize("mtte.howto"), help: game.i18n.localize("mtte.howtoToolTip") }
       ]
     })
